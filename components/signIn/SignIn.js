852f2fd1d54f2de95eef35ee8824947d9ab898e2
Sajal Talukder <76643944+SajalTalukder@users.noreply.github.com>
2021-09-23 10:03:19 -0700

    Update SignIn.js

--- components/signIn/SignIn.js
+++ components/signIn/SignIn.js
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/client";
 
 function SignIn() {
   const signInHandler = () => {
-    signIn(null, { callbackUrl: "http://localhost:3000/movies" });
+    signIn(null, { callbackUrl: "netflix-clone-psi-blue.vercel.app/movies" });
   };
   return (
     <div className={cls["signin"]}>
